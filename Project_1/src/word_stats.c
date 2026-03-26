9d0da06ae05028f09e03fa943ee8da66b20be291
radiantegg <2173590735@qq.com>
2026-03-26 20:30:26 +0800

    feat: add get_initials and get_longest_word

--- Project_1/src/word_stats.c
+++ Project_1/src/word_stats.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 #include "word_stats.h"
+#include "word_cleaner.h"
 
 int count_word(char *s)
 {
@@ -11,4 +13,52 @@ int count_word(char *s)
         p++;
     }
     return count + 1;
+}
+
+void get_initials(char *s, char *d)
+{
+    char *p = s;
+    char *t = d;
+    char last = ' ';
+    while (*p != '\0') {
+        if (last == ' ') {
+            *t = *p;
+            t++;
+        }
+        last = *p;
+        p++;
+    }
+    *t = '\0';
+}
+
+void get_longest_word(char *s, char *d)
+{
+    char word[MAXLINE][MAXLINE];
+    char *p = s;
+    int row, col, i, index;
+    int length, max_length = 0;
+    for (row = 0;;row++) {
+        col = 0;
+        while (*p != ' ' && *p != '\0') {
+            word[row][col++] = *p;
+            p++;
+        }
+        word[row][col] = '\0';
+
+        if (*p == '\0') {
+            break;
+        } else {
+            p++;
+        }
+    }
+
+    for (i = 0; i <= row; i++) {
+        length = strlen(word[i]);
+
+        if (length > max_length) {
+            max_length = length;
+            index = i;
+        }
+    }
+    strcpy(d, word[index]);
 }
--- Project_1/tests/test_word_stats.c
+++ Project_1/tests/test_word_stats.c
@@ -5,7 +5,12 @@
 int main()
 {
     char s[100];
+    char initials[100];
+    char longest_word[100];
     read_text(s);
-    printf("%d\n", count_word(s));
+    // printf("%d\n", count_word(s));
+    // get_initials(s,initials);
+    get_longest_word(s, longest_word);
+    printf("%s\n", longest_word);
     return 0;
 }
